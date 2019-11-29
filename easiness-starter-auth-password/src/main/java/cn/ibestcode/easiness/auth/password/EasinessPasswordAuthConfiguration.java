888af36f2a43fc75ed916519fdf0eee9382980ff
wfso <4616663376@qq.com>
2019-11-29 20:55:11 +0800

    refactor(easiness-starter-auth-password): 启动效率优化

--- easiness-starter-auth-password/src/main/java/cn/ibestcode/easiness/auth/password/EasinessPasswordAuthConfiguration.java
+++ easiness-starter-auth-password/src/main/java/cn/ibestcode/easiness/auth/password/EasinessPasswordAuthConfiguration.java
@@ -1,12 +1,10 @@
 package cn.ibestcode.easiness.auth.password;
 
-import cn.ibestcode.easiness.auth.EnableEasinessAuth;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-@EnableEasinessAuth
 @ComponentScan
 public class EasinessPasswordAuthConfiguration {
 }
