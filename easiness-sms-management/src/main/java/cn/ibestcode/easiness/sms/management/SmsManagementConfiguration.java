bd113313ac7a2e119a20da6b7be925814af34a0b
wfso <4616663376@qq.com>
2019-12-13 23:47:11 +0800

    feat(easiness-sms-management): 创建 easiness-sms-management 模块

---
+++ easiness-sms-management/src/main/java/cn/ibestcode/easiness/sms/management/SmsManagementConfiguration.java
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2019 the original author or authors.
+ * <p>
+ * Licensed to the IBESTCODE under one or more agreements.
+ * The IBESTCODE licenses this file to you under the MIT license.
+ * See the LICENSE file in the project root for more information.
+ */
+package cn.ibestcode.easiness.sms.management;
+
+import cn.ibestcode.easiness.sendsms.EnableEasinessSendSms;
+
+
+/**
+ * @author WFSO (仵士杰)
+ * create by WFSO (仵士杰) at 2019/11/20 19:13
+ */
+@EnableEasinessSendSms
+public class SmsManagementConfiguration {
+}
