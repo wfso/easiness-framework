d78611bbc5cb3ef3e8534a4c0534e3b90c004797
wfso <461663376@qq.com>
2020-01-11 20:58:13 +0800

    feat(easiness-core): Controller基类的方法添加@ApiOperation注解

--- easiness-core/src/main/java/cn/ibestcode/easiness/core/base/controller/BaseController.java
+++ easiness-core/src/main/java/cn/ibestcode/easiness/core/base/controller/BaseController.java
@@ -3,6 +3,7 @@ package cn.ibestcode.easiness.core.base.controller;
 import cn.ibestcode.easiness.core.base.service.Service;
 import cn.ibestcode.easiness.core.paging.PageableGenerator;
 import cn.ibestcode.easiness.core.query.filter.FilterGenerator;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,18 +12,21 @@ public abstract class BaseController<T, ID, FG extends FilterGenerator, PG exten
 
   @Override
   @PostMapping
+  @ApiOperation("添加XXXX")
   public T add(@RequestBody T t) {
     return getService().create(t);
   }
 
   @Override
   @PutMapping
+  @ApiOperation("修改XXXX")
   public T edit(@RequestBody T t) {
     return getService().update(t);
   }
 
   @Override
-  @DeleteMapping(path = "{id}")
+  @DeleteMapping("{id}")
+  @ApiOperation("删除XXXX")
   public T remove(@PathVariable("id") ID id) {
     T t = getService().getById(id);
     getService().removeById(id);
@@ -30,19 +34,22 @@ public abstract class BaseController<T, ID, FG extends FilterGenerator, PG exten
   }
 
   @Override
-  @GetMapping(path = "{id}")
+  @GetMapping("{id}")
+  @ApiOperation("XXXX详细")
   public T info(@PathVariable("id") ID id) {
     return getService().getById(id);
   }
 
   @Override
-  @PostMapping(path = "page")
+  @PostMapping("page")
+  @ApiOperation("XXXX列表-分页-POST")
   public Page<T> postPage(@RequestBody FG filterGenerator, PG pageableGenerator) {
     return getPage(filterGenerator, pageableGenerator);
   }
 
   @Override
-  @GetMapping()
+  @GetMapping
+  @ApiOperation("XXXX列表-分页-GET")
   public Page<T> getPage(FG filterGenerator, PG pageableGenerator) {
     return getService().getPage(filterGenerator.generateFilter(), pageableGenerator.generatePageable());
   }
--- easiness-core/src/main/java/cn/ibestcode/easiness/core/base/controller/UuidBaseController.java
+++ easiness-core/src/main/java/cn/ibestcode/easiness/core/base/controller/UuidBaseController.java
@@ -4,6 +4,7 @@ import cn.ibestcode.easiness.core.base.model.UuidBaseJpaModel;
 import cn.ibestcode.easiness.core.base.service.UuidBaseJpaService;
 import cn.ibestcode.easiness.core.paging.PageableGenerator;
 import cn.ibestcode.easiness.core.query.filter.FilterGenerator;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,18 +14,21 @@ public abstract class UuidBaseController<T extends UuidBaseJpaModel, FG extends 
 
   @Override
   @PostMapping
+  @ApiOperation("添加XXXX")
   public T add(@RequestBody T t) {
     return getService().create(t);
   }
 
   @Override
   @PutMapping
+  @ApiOperation("修改XXXX")
   public T edit(@RequestBody T t) {
     return getService().update(t);
   }
 
   @Override
-  @DeleteMapping(path = "{uuid}")
+  @DeleteMapping("{uuid}")
+  @ApiOperation("删除XXXX")
   public T remove(@PathVariable("uuid") String uuid) {
     T t = getService().getByUuid(uuid);
     getService().removeByUuid(uuid);
@@ -32,19 +36,22 @@ public abstract class UuidBaseController<T extends UuidBaseJpaModel, FG extends 
   }
 
   @Override
-  @GetMapping(path = "{uuid}")
+  @GetMapping("{uuid}")
+  @ApiOperation("XXXX详情")
   public T info(@PathVariable("uuid") String uuid) {
     return getService().getByUuid(uuid);
   }
 
   @Override
-  @PostMapping(path = "page")
+  @PostMapping("page")
+  @ApiOperation("XXXX列表-分页-POST")
   public Page<T> postPage(@RequestBody FG filterGenerator, PG pageableGenerator) {
     return getPage(filterGenerator, pageableGenerator);
   }
 
   @Override
-  @GetMapping()
+  @GetMapping
+  @ApiOperation("XXXX列表-分页-GET")
   public Page<T> getPage(FG filterGenerator, PG pageableGenerator) {
     return getService().getPage(filterGenerator.generateFilter(), pageableGenerator.generatePageable());
   }
