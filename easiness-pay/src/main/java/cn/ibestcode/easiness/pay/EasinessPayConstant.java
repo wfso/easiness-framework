0f5d3a1d1f100abd927c75ed28bba0880c02668a
wfso <461663376@qq.com>
2020-03-05 21:51:38 +0800

    feat(easiness-pay): 支付过期时间限制的注释

--- easiness-pay/src/main/java/cn/ibestcode/easiness/pay/EasinessPayConstant.java
+++ easiness-pay/src/main/java/cn/ibestcode/easiness/pay/EasinessPayConstant.java
@@ -5,5 +5,6 @@ public class EasinessPayConstant {
   public static final String ASYNC_NOTIFY_URL_PREFIX = "/api/easiness/pay/notify";
 
   // 支付的默认过期时间
+  // 需要两个小时以内，否则微信支付会有bug，因为微信的 prepay_id 的有效期最长为两个小时
   public static final String PAY_DEFAULT_EXPIRE = "easiness.pay.expire";
 }
