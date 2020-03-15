014798aa5488b3e2c5b77f384817352dee9c5740
wfso <461663376@qq.com>
2020-03-15 20:16:27 +0800

    fix(easiness-backup-entity): supports 方法的参数不能用泛型

--- easiness-backup-entity/src/main/java/cn/ibestcode/easiness/backup/entity/handler/BackupsHandler.java
+++ easiness-backup-entity/src/main/java/cn/ibestcode/easiness/backup/entity/handler/BackupsHandler.java
@@ -3,7 +3,7 @@ package cn.ibestcode.easiness.backup.entity.handler;
 import cn.ibestcode.easiness.backup.entity.BackupMetadata;
 
 public interface BackupsHandler<T> {
-  boolean supports(T object);
+  boolean supports(Object object);
 
   void backup(T entity, BackupMetadata metadata);
 }
