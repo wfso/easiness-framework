c75a280602e403a61fa047b32ee403f54680a0c4
wfso <461663376@qq.com>
2020-04-17 22:45:17 +0800

    feat(easiness-starter-shiro-auth): DefaultAuthorizationExceptionHandler

--- easiness-starter-shiro-auth/src/main/java/cn/ibestcode/easiness/shiro/auth/handler/DefaultAuthorizationExceptionHandler.java
+++ easiness-starter-shiro-auth/src/main/java/cn/ibestcode/easiness/shiro/auth/handler/DefaultAuthorizationExceptionHandler.java
@@ -7,14 +7,23 @@
  */
 package cn.ibestcode.easiness.shiro.auth.handler;
 
+import cn.ibestcode.easiness.auth.model.EasinessPermission;
+import cn.ibestcode.easiness.auth.model.EasinessRole;
+import cn.ibestcode.easiness.auth.service.EasinessPermissionService;
+import cn.ibestcode.easiness.auth.service.EasinessRoleService;
 import cn.ibestcode.easiness.exception.handler.AbstractExceptionHandler;
 import cn.ibestcode.easiness.exception.properties.EasinessExceptionTipsProperties;
 import org.apache.shiro.authz.AuthorizationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class DefaultAuthorizationExceptionHandler extends AbstractExceptionHandler {
@@ -22,9 +31,81 @@ public class DefaultAuthorizationExceptionHandler extends AbstractExceptionHandl
   @Autowired
   private EasinessExceptionTipsProperties tipsProperties;
 
-  @ExceptionHandler(AuthorizationException.class)
+  protected EasinessPermissionService permissionService;
+
+  protected EasinessRoleService roleService;
+
   @ResponseBody
+  @ExceptionHandler(AuthorizationException.class)
   ResponseEntity<Object> defaultAuthorizationExceptionHandle(Throwable throwable) {
-    return commonExceptionHandler(throwable, tipsProperties.getCodeName(), tipsProperties.getMsgName());
+    Map<String, Object> result = new HashMap<>();
+    String code = throwable.getMessage();
+    String msg = null;
+
+    // 无登录
+    if (code.startsWith("This subject is anonymous")
+      || code.startsWith("The current Subject is not authenticated")) {
+      code = "not-logged-in";
+      String msgKey = throwable.getClass().getSimpleName() + "." + code;
+      if (messageSource != null) {
+        msg = messageSource.getMessage(msgKey, null, code, LocaleContextHolder.getLocale());
+      }
+    }
+
+    // 无权限
+    else if (code.startsWith("Subject does not have permission")) {
+      code = code.substring(34, code.length() - 1);
+      if (messageSource != null) {
+        String msgParam = "";
+        if (permissionService != null) {
+          EasinessPermission permission = permissionService.getByCode(code);
+          msgParam = (permission == null ? code : permission.getName());
+        }
+        String msgKey = throwable.getClass().getSimpleName() + ".subject-does-not-have-permission";
+        code = "subject-does-not-have-permission[" + code + "]";
+        msg = messageSource.getMessage(msgKey, new String[]{msgParam}, code, LocaleContextHolder.getLocale());
+      }
+    }
+
+    // 无角色
+    else if (code.startsWith("Subject does not have role")) {
+      code = code.substring(28, code.length() - 1);
+      if (messageSource != null) {
+        String msgParam = "";
+        if (roleService != null) {
+          EasinessRole role = roleService.getByCode(code);
+          msgParam = (role == null ? code : role.getName());
+        }
+        String msgKey = throwable.getClass().getSimpleName() + ".subject-does-not-have-role";
+        code = "subject-does-not-have-role[" + code + "]";
+        msg = messageSource.getMessage(msgKey, new String[]{msgParam}, code, LocaleContextHolder.getLocale());
+      }
+    }
+
+    // guest-only
+    else if (code.startsWith("Attempting to perform a guest-only operation")) {
+      code = "guest-only";
+      String msgKey = throwable.getClass().getSimpleName() + "." + code;
+      if (messageSource != null) {
+        msg = messageSource.getMessage(msgKey, null, code, LocaleContextHolder.getLocale());
+      }
+    }
+
+    // 自定义访问权限异常 SystemPermissionException
+    else if (code.startsWith("SPE-")) {
+      String msgKey = throwable.getClass().getSimpleName() + "." + code;
+      if (messageSource != null) {
+        msg = messageSource.getMessage(msgKey, null, code, LocaleContextHolder.getLocale());
+      }
+    }
+
+    // 当 msg 没有被赋值时
+    if (msg == null) {
+      msg = code;
+    }
+
+    result.put(tipsProperties.getCodeName(), code);
+    result.put(tipsProperties.getMsgName(), msg);
+    return new ResponseEntity(result, HttpStatus.OK);
   }
 }
