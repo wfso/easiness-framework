1be3302610025cb914b0fd902263c29aca5097f6
wfso <4616663376@qq.com>
2020-10-05 13:08:13 +0800

    - 添加 HttpSession 管理功能

--- easiness-starter-springetc/src/main/java/cn/ibestcode/easiness/spring/utils/ServletUtil.java
+++ easiness-starter-springetc/src/main/java/cn/ibestcode/easiness/spring/utils/ServletUtil.java
@@ -6,6 +6,7 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class ServletUtil {
   public static ServletRequestAttributes getServletRequestAttributes() {
@@ -22,6 +23,10 @@ public class ServletUtil {
 
   public static ServletContext getServletContext(){
     return getHttpServletRequest().getServletContext();
+  }
+
+  public static HttpSession getHttpSession(){
+    return getHttpServletRequest().getSession();
   }
 
 }
