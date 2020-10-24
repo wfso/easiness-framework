642ea092a87f8380238cf384912178f76e4f9521
wfso <461663376@qq.com>
2020-10-24 10:24:21 +0800

    feat(easiness-utils): Map 的操作 移动到 MapUtil 中

--- easiness-utils/src/main/java/cn/ibestcode/easiness/utils/MapUtil.java
+++ easiness-utils/src/main/java/cn/ibestcode/easiness/utils/MapUtil.java
@@ -8,14 +8,15 @@
 
 package cn.ibestcode.easiness.utils;
 
-import org.springframework.beans.BeanWrapper;
-import org.springframework.beans.BeanWrapperImpl;
-import org.springframework.util.ObjectUtils;
+import org.springframework.beans.*;
+import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
+import org.springframework.util.ClassUtils;
 
 import java.beans.PropertyDescriptor;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.*;
 
 /**
  * @author WFSO (仵士杰)
@@ -24,23 +25,191 @@ import java.util.TreeMap;
 public class MapUtil {
   public static Map<String, String> monolayerObjectToMap(Object object) {
     Map<String, String> map = new HashMap<>();
-    monolayerFillMap(object, map);
+    monolayerFillMap(object, map, null);
     return map;
   }
 
   public static TreeMap<String, String> monolayerObjectToTreeMap(Object object) {
     TreeMap<String, String> treeMap = new TreeMap<>();
-    monolayerFillMap(object, treeMap);
+    monolayerFillMap(object, treeMap, null);
     return treeMap;
   }
 
-  protected static void monolayerFillMap(Object object, Map<String, String> map) {
+  public static Map<String, String> monolayerObjectToMap(Object object, Class<?> editable) {
+    Map<String, String> map = new HashMap<>();
+    monolayerFillMap(object, map, editable);
+    return map;
+  }
+
+  public static TreeMap<String, String> monolayerObjectToTreeMap(Object object, Class<?> editable) {
+    TreeMap<String, String> treeMap = new TreeMap<>();
+    monolayerFillMap(object, treeMap, editable);
+    return treeMap;
+  }
+
+
+  public static Map<String, String> monolayerObjectToMap(Object object, String... ignoreProperties) {
+    Map<String, String> map = new HashMap<>();
+    monolayerFillMap(object, map, null, ignoreProperties);
+    return map;
+  }
+
+  public static TreeMap<String, String> monolayerObjectToTreeMap(Object object, String... ignoreProperties) {
+    TreeMap<String, String> treeMap = new TreeMap<>();
+    monolayerFillMap(object, treeMap, null, ignoreProperties);
+    return treeMap;
+  }
+
+  protected static void monolayerFillMap(Object object, Map<String, String> map, Class<?> editable, String... ignoreProperties) {
+    Class<?> actualEditable = object.getClass();
+    if (editable != null) {
+      if (!editable.isInstance(object)) {
+        throw new IllegalArgumentException("Target class [" + object.getClass().getName() +
+          "] not assignable to Editable class [" + editable.getName() + "]");
+      }
+      actualEditable = editable;
+    }
     final BeanWrapper sourceWrapper = new BeanWrapperImpl(object);
-    PropertyDescriptor[] pds = sourceWrapper.getPropertyDescriptors();
+    PropertyDescriptor[] pds = BeanUtils.getPropertyDescriptors(actualEditable);
+    List<String> ignoreList = (ignoreProperties != null ? Arrays.asList(ignoreProperties) : null);
     for (PropertyDescriptor pd : pds) {
-      Object propertyValue = sourceWrapper.getPropertyValue(pd.getName());
-      if (!ObjectUtils.isEmpty(propertyValue)) {
-        map.put(pd.getName(), propertyValue.toString());
+      if ((ClassUtils.isPrimitiveOrWrapper(pd.getPropertyType()) || String.class.isAssignableFrom(pd.getPropertyType()))
+        && (ignoreList == null || !ignoreList.contains(pd.getName()))) {
+        Object propertyValue = sourceWrapper.getPropertyValue(pd.getName());
+        if (propertyValue != null) {
+          map.put(pd.getName(), propertyValue.toString());
+        }
+      }
+    }
+  }
+
+
+  public static Map<String, Object> objectToMap(Object object) {
+    Map<String, Object> map = new HashMap<>();
+    fillMap(object, map, null);
+    return map;
+  }
+
+  public static TreeMap<String, Object> objectToTreeMap(Object object) {
+    TreeMap<String, Object> treeMap = new TreeMap<>();
+    fillMap(object, treeMap, null);
+    return treeMap;
+  }
+
+  public static Map<String, Object> objectToMap(Object object, Class<?> editable) {
+    Map<String, Object> map = new HashMap<>();
+    fillMap(object, map, editable);
+    return map;
+  }
+
+  public static TreeMap<String, Object> objectToTreeMap(Object object, Class<?> editable) {
+    TreeMap<String, Object> treeMap = new TreeMap<>();
+    fillMap(object, treeMap, editable);
+    return treeMap;
+  }
+
+  public static Map<String, Object> objectToMap(Object object, String... ignoreProperties) {
+    Map<String, Object> map = new HashMap<>();
+    fillMap(object, map, null, ignoreProperties);
+    return map;
+  }
+
+  public static TreeMap<String, Object> objectToTreeMap(Object object, String... ignoreProperties) {
+    TreeMap<String, Object> treeMap = new TreeMap<>();
+    fillMap(object, treeMap, null, ignoreProperties);
+    return treeMap;
+  }
+
+
+  protected static void fillMap(Object object, Map<String, Object> map, @Nullable Class<?> editable, @Nullable String... ignoreProperties) {
+    Class<?> actualEditable = object.getClass();
+    if (editable != null) {
+      if (!editable.isInstance(object)) {
+        throw new IllegalArgumentException("Target class [" + object.getClass().getName() +
+          "] not assignable to Editable class [" + editable.getName() + "]");
+      }
+      actualEditable = editable;
+    }
+    final BeanWrapper sourceWrapper = new BeanWrapperImpl(object);
+    PropertyDescriptor[] pds = BeanUtils.getPropertyDescriptors(actualEditable);
+    List<String> ignoreList = (ignoreProperties != null ? Arrays.asList(ignoreProperties) : null);
+    for (PropertyDescriptor pd : pds) {
+      if (ignoreList == null || !ignoreList.contains(pd.getName())) {
+        Object propertyValue = sourceWrapper.getPropertyValue(pd.getName());
+        map.put(pd.getName(), propertyValue);
+      }
+    }
+  }
+
+
+  protected static void mapToObject(Map source, Object target, @Nullable Class<?> editable,
+                                    @Nullable String... ignoreProperties) throws BeansException {
+    Assert.notNull(source, "Source must not be null");
+    Assert.notNull(target, "Target must not be null");
+
+    Class<?> actualEditable = target.getClass();
+    if (editable != null) {
+      if (!editable.isInstance(target)) {
+        throw new IllegalArgumentException("Target class [" + target.getClass().getName() +
+          "] not assignable to Editable class [" + editable.getName() + "]");
+      }
+      actualEditable = editable;
+    }
+    PropertyDescriptor[] targetPds = BeanUtils.getPropertyDescriptors(actualEditable);
+    List<String> ignoreList = (ignoreProperties != null ? Arrays.asList(ignoreProperties) : null);
+
+    for (PropertyDescriptor targetPd : targetPds) {
+      Method writeMethod = targetPd.getWriteMethod();
+      if (writeMethod != null && (ignoreList == null || !ignoreList.contains(targetPd.getName()))) {
+        Object value = source.get(targetPd.getName());
+        Class writeClass = writeMethod.getParameterTypes()[0];
+        if (value != null) {
+          if (ClassUtils.isAssignable(writeClass, value.getClass())) {
+            try {
+              if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
+                writeMethod.setAccessible(true);
+              }
+              writeMethod.invoke(target, value);
+            } catch (Throwable ex) {
+              throw new FatalBeanException(
+                "Could not copy property '" + targetPd.getName() + "' from source to target", ex);
+            }
+          } else if ((ClassUtils.isPrimitiveWrapper(value.getClass()) || String.class.isAssignableFrom(value.getClass()))
+            && (ClassUtils.isPrimitiveOrWrapper(writeClass) || writeClass == String.class)) {
+            String sv = value.toString();
+            if (writeClass.isPrimitive()) {
+              writeClass = ClassUtils.resolvePrimitiveIfNecessary(writeClass);
+            }
+
+            try {
+              if (String.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, sv);
+              } else if (Integer.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, Integer.valueOf(sv));
+              } else if (Float.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, Float.valueOf(sv));
+              } else if (Double.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, Double.valueOf(sv));
+              } else if (Character.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, sv.charAt(0));
+              } else if (Short.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, Short.valueOf(sv));
+              } else if (Long.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, Long.valueOf(sv));
+              } else if (Byte.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, Byte.valueOf(sv));
+              } else if (Boolean.class.isAssignableFrom(writeClass)) {
+                writeMethod.invoke(target, Boolean.valueOf(sv));
+              } else {
+                throw new FatalBeanException(
+                  "Could not copy property '" + targetPd.getName() + "' from source to target");
+              }
+            } catch (Throwable ex) {
+              throw new FatalBeanException(
+                "Could not copy property '" + targetPd.getName() + "' from source to target", ex);
+            }
+          }
+        }
       }
     }
   }
--- easiness-utils/src/main/java/cn/ibestcode/easiness/utils/SpringBeanUtilsExt.java
+++ easiness-utils/src/main/java/cn/ibestcode/easiness/utils/SpringBeanUtilsExt.java
@@ -1,15 +1,16 @@
 package cn.ibestcode.easiness.utils;
 
-import org.springframework.beans.*;
-import org.springframework.lang.Nullable;
-import org.springframework.util.Assert;
-import org.springframework.util.ClassUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+import org.springframework.beans.BeansException;
 import org.springframework.util.ObjectUtils;
 
 import java.beans.PropertyDescriptor;
-import java.lang.reflect.Method;
-import java.lang.reflect.Modifier;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class SpringBeanUtilsExt extends BeanUtils {
 
@@ -53,20 +54,20 @@ public class SpringBeanUtilsExt extends BeanUtils {
 
   // 忽略 null 进行 copy
   public static void copyPropertiesIgnoreNull(Object source, Object target) throws BeansException {
-    BeanUtils.copyProperties(source, target, getNullPropertyNames(source).toArray(new String[0]));
+    copyProperties(source, target, getNullPropertyNames(source).toArray(new String[0]));
   }
 
 
   // 忽略 “空”（null，空字符串，空集合等） 进行 copy
   public static void copyPropertiesIgnoreEmpty(Object source, Object target) throws BeansException {
-    BeanUtils.copyProperties(source, target, getEmptyPropertyNames(source).toArray(new String[0]));
+    copyProperties(source, target, getEmptyPropertyNames(source).toArray(new String[0]));
   }
 
   // 忽略 null 进行 copy
   public static void copyPropertiesIgnoreNull(Object source, Object target, String... ignoreProperties) throws BeansException {
     Set<String> noValuePropertySet = getNullPropertyNames(source);
     noValuePropertySet.addAll(Arrays.asList(ignoreProperties));
-    BeanUtils.copyProperties(source, target, noValuePropertySet.toArray(new String[0]));
+    copyProperties(source, target, noValuePropertySet.toArray(new String[0]));
   }
 
 
@@ -74,79 +75,7 @@ public class SpringBeanUtilsExt extends BeanUtils {
   public static void copyPropertiesIgnoreEmpty(Object source, Object target, String... ignoreProperties) throws BeansException {
     Set<String> noValuePropertySet = getNullPropertyNames(source);
     noValuePropertySet.addAll(Arrays.asList(ignoreProperties));
-    BeanUtils.copyProperties(source, target, noValuePropertySet.toArray(new String[0]));
+    copyProperties(source, target, noValuePropertySet.toArray(new String[0]));
   }
 
-
-  private static void copyProperties(Map source, Object target, @Nullable Class<?> editable,
-                                     @Nullable String... ignoreProperties) throws BeansException {
-    Assert.notNull(source, "Source must not be null");
-    Assert.notNull(target, "Target must not be null");
-
-    Class<?> actualEditable = target.getClass();
-    if (editable != null) {
-      if (!editable.isInstance(target)) {
-        throw new IllegalArgumentException("Target class [" + target.getClass().getName() +
-          "] not assignable to Editable class [" + editable.getName() + "]");
-      }
-      actualEditable = editable;
-    }
-    PropertyDescriptor[] targetPds = getPropertyDescriptors(actualEditable);
-    List<String> ignoreList = (ignoreProperties != null ? Arrays.asList(ignoreProperties) : null);
-
-    for (PropertyDescriptor targetPd : targetPds) {
-      Method writeMethod = targetPd.getWriteMethod();
-      if (writeMethod != null && (ignoreList == null || !ignoreList.contains(targetPd.getName()))) {
-        Object value = source.get(targetPd.getName());
-        Class writeClass = writeMethod.getParameterTypes()[0];
-        if (value != null) {
-          if (ClassUtils.isAssignable(writeClass, value.getClass())) {
-            try {
-              if (!Modifier.isPublic(writeMethod.getDeclaringClass().getModifiers())) {
-                writeMethod.setAccessible(true);
-              }
-              writeMethod.invoke(target, value);
-            } catch (Throwable ex) {
-              throw new FatalBeanException(
-                "Could not copy property '" + targetPd.getName() + "' from source to target", ex);
-            }
-          } else if ((ClassUtils.isPrimitiveWrapper(value.getClass()) || value.getClass() == String.class)
-            && (ClassUtils.isPrimitiveOrWrapper(writeClass) || writeClass == String.class)) {
-            String sv = value.toString();
-            if (writeClass.isPrimitive()) {
-              writeClass = ClassUtils.resolvePrimitiveIfNecessary(writeClass);
-            }
-
-            try {
-              if (String.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, sv);
-              } else if (Integer.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, Integer.valueOf(sv));
-              } else if (Float.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, Float.valueOf(sv));
-              } else if (Double.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, Double.valueOf(sv));
-              } else if (Character.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, sv.charAt(0));
-              } else if (Short.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, Short.valueOf(sv));
-              } else if (Long.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, Long.valueOf(sv));
-              } else if (Byte.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, Byte.valueOf(sv));
-              } else if (Boolean.class.isAssignableFrom(writeClass)) {
-                writeMethod.invoke(target, Boolean.valueOf(sv));
-              } else {
-                throw new FatalBeanException(
-                  "Could not copy property '" + targetPd.getName() + "' from source to target");
-              }
-            } catch (Throwable ex) {
-              throw new FatalBeanException(
-                "Could not copy property '" + targetPd.getName() + "' from source to target", ex);
-            }
-          }
-        }
-      }
-    }
-  }
 }
